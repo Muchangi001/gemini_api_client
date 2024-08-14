213ba19df5154ef275cdddaeaf7883f9f33bc20c
Muchangi001 <andrewmuchangi254@gmail.com>
2024-08-14 18:47:08 +0300

    Add functionality to store user and AI responses to filesystem

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# OVERVIEW
+**A C++ client that consumes the Google Gemini API and stores the prompts and responses inside 'gemini_logs.txt' located in the 'logs' folder which will be created if it does exist.**<br>
+
+
 # LIBRARY USED
 **C++ REST SDK (Casablanca)** for sending post request.
 
@@ -8,10 +12,11 @@
 
 # RUNNING THE PROGRAM
 Make sure to install C++ REST SDK library in your system.<br>
-You also have to replace the value of the **apiKey** variable inside the main function with your actual **api key**.<br>
 Run the **"gemini"** executable in the bin folder along with your prompt **(Only for linux users)**.<br>
-**i.e, ./gemini Hello there,can you explain how AI works? or ./gemini "Hello there,can you explain how AI works?".I prefer to use the latter one because certain symbols have special meaning to the shell e.g. '?'**.
-Windows and Mac users will have to compile the code themselves.
+**i.e, ./gemini Hello there,can you explain how AI works?** or <br>
+**./gemini "Hello there,can you explain how AI works?".I prefer to use the latter one because certain symbols have special meaning to the shell e.g. '?'**.
+Windows and Mac users will have to compile the code themselves.<br>
+If you want to compile the project yourself you have to replace the value of the **apiKey** variable inside the **main** function with your actual **api key**.
 
 # COMPILING THE CODE
 For G++ compiler run -> **g++ src/gemini.cpp src/main.cpp -o bin/gemini -lcpprest -lssl -lcrypto -lboost_system**
--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,38 @@
 
 int main(int argc, char *argv[]) {
 
+    const std::string logsDir = "logs";
+    const std::string logFilePath = logsDir + "/gemini_logs.txt";
     const std::string uri = "https://generativelanguage.googleapis.com";
-    std::string apiKey = "AIzaSyCFDYHepi-dXQ-n2YZpR_sx6OlAgVDbgOs";
+    std::string apiKey = "AIzaSyCFDYHepi-dXQ-n2YZpR_sx6OlAgVDbgOs"; //replace with your actual api key
+
+    //check if the log file exists
+    if (!std::filesystem::exists(logsDir)) {
+        std::cout << "creating new 'logs' directory" << std::endl;
+        std::filesystem::create_directory(logsDir);
+        std::cout << "creating new 'logs/gemini_logs.txt' file" << std::endl << std::endl;
+    } else {
+        std::cout << "writing to logs/gemini_logs.txt" << std::endl << std::endl;
+    }
+
+    //opening the log file in append mode
+    std::ofstream logfile;
+    logfile.open(logFilePath, std::ios::app);
+
+    //checking if the log file is open
+    if (!logfile) {
+        std::cerr << "Error : Could not open the file for writing" << std::endl;
+        return 1;
+    }
 
     //storing input
     std::string prompt;
     prompt = readInput(prompt, argv, argc);
 
-    //http client
+    //creating http client
     web::http::client::http_client client (U(uri));
 
-    //json payload
+    //setting up json payload for the post request
     web::json::value postData = web::json::value::object({{
         U("contents"), web::json::value::array({
             web::json::value::object({{
@@ -25,20 +46,33 @@ int main(int argc, char *argv[]) {
         })
     }});
 
-    //http request
+    //create and configure http post request
     web::http::http_request request(web::http::methods::POST);
     request.set_request_uri(U("/v1beta/models/gemini-1.5-flash-latest:generateContent?key=") + U(apiKey));
     request.headers().set_content_type(U("application/json"));
     request.set_body(postData);
 
-    //send request
-    client.request(request).then([](web::http::http_response response){
+    //send request and process response
+    client.request(request).then([&](web::http::http_response response){
         if (response.status_code() == web::http::status_codes::OK) {
             auto body = response.extract_json().get();
             auto text = body[U("candidates")][0][U("content")][U("parts")][0][U("text")].as_string();
+
+            //get the current time and format it
+            std::tm currentTime = getCurrentTime();
+            logfile << "[TIMESTAMP : "<< std::put_time(&currentTime, "%d-%m-%Y %H:%M:%S") << "]" << std::endl;
+
+            //write the response to the log file
+            logfile << "PROMPT => " << prompt << std::endl;
+            logfile << "RESPONSE ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓" << std::endl << text << std::endl << std::endl << std::endl;
             std::cout << text <<std::endl;
-        }else std::cerr << "Request failed with status code: " << response.status_code() << std::endl;
+        } else {
+            std::cerr << "Request failed with status code: " << response.status_code() << std::endl;
+        }
     }).wait();
+
+    //closing the log file
+    logfile.close();
 
     return 0;
 }
